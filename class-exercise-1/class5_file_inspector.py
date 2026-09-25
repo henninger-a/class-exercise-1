5073628d4bd842bdcd7df440b4de28d14896cbb4
Addison_Henninger <henninger.a@northeastern.edu>
2026-09-25 15:11:09 -0400

    Built file inspector with modules

---
+++ class-exercise-1/class5_file_inspector.py
@@ -0,0 +1,59 @@
+import argparse
+import logging
+import sys
+
+from class5_file_utils import inspect_file, inspect_extension
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(name)s — %(message)s",
+    datefmt="%H:%M:%S"
+)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Inspect a text file"
+    )
+    parser.add_argument(
+        "--input",
+        "-i",
+        required=True,
+        help="Path to a .txt file"
+    )
+    args = parser.parse_args()
+
+    # TODO 1: Call inspect_file() inside a try block.
+
+    try:
+        file_info = inspect_file(args.input)
+
+    # TODO 2: Catch FileNotFoundError.
+    #         Log an ERROR message (e.g. file not found), and 
+    #         exit with sys.exit(1).
+
+    except FileNotFoundError:
+        logger.error(f"File doesn't exist: File not found: {args.input}")
+        sys.exit(1)
+
+    # TODO 3: Call inspect_extension() inside a separate try block.
+
+    try:
+        inspect_extension(inspect_file(args.input))
+
+    # TODO 4: Catch ValueError.
+    #         Log an ERROR message (e.g. unsupported format), and 
+    #         exit with sys.exit(1).
+
+    except ValueError:
+        logger.error(f"Unsupported format: Unsupported text format: {file_info['Extension']}'")
+        sys.exit(1)
+
+    # TODO 5: Log an INFO message containing the
+    #         file name and extension.
+
+    logger.info(f'File: {args.input} Extension: {file_info['Extension']}')
+
+if __name__ == "__main__":
+    main()
---
+++ class-exercise-1/class5_file_utils.py
@@ -0,0 +1,42 @@
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def inspect_file(filepath_str):
+    """Return basic information about an existing file."""
+    # TODO 1: Create a Path object.
+
+    path = Path(filepath_str)
+
+    # TODO 2: If the path is not a file:
+    #         Log an ERROR message.
+    #         Raise FileNotFoundError (e.g. file not found).
+
+    if path.is_file() is False:
+        logger.error(f'File not found: {filepath_str}')
+        raise FileNotFoundError(f'File not found {filepath_str}')
+
+    # TODO 3: Return a dictionary containing:
+    #         name and extension.
+
+    return {'Name': path.stem, 'Extension': path.suffix}
+
+
+def inspect_extension(file_info):
+    """Confirm that the file uses a supported text extension."""
+    supported_extension = ".txt"
+
+    # TODO 4: If file_info["extension"] does not equal
+    #         supported_extension:
+    #         Log an ERROR message (e.g. unsupported format).
+    #         Raise ValueError.
+
+    if file_info['Extension'] != supported_extension:
+        logger.error(f'Unsupported text format: {file_info['Extension']}')
+        raise ValueError(f'Unsupported text format: {file_info['Extension']}')
+
+    # TODO 5: Return file_info.
+
+    return file_info
